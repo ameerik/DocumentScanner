c711153c075b0aaf7cef87c3ad3831adc4aeb21b
ameerik <ameerinderkajee7@gmail.com>
2025-04-01 16:14:32 +0200

    Added Contour Identification

--- scan.py
+++ scan.py
@@ -12,7 +12,6 @@ ap.add_argument("-i", "--image", required = True,
 	help = "Path to the image to be scanned")
 args = vars(ap.parse_args())
 
-
 # load the image and compute the ratio of the old height
 # to the new height, clone it, and resize it
 image = cv2.imread(args["image"])
@@ -30,5 +29,30 @@ edged = cv2.Canny(gray, 75, 200)
 print("STEP 1: Edge Detection")
 cv2.imshow("Image", image)
 cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# find the contours in the edged image, keeping only the
+# largest ones, and initialize the screen contour
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+# loop over the contours
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+	# if our approximated contour has four points, then we
+	# can assume that we have found our screen
+	if len(approx) == 4:
+		screenCnt = approx
+		break
+	
+# show the contour (outline) of the piece of paper
+print("STEP 2: Find contours of paper")
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outline", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
